61c723024ae8822acd25667d1def7754bd790201
Mathews Onyango <159943867+Maat98@users.noreply.github.com>
2024-03-10 22:52:21 +0300

    function assgnment week3

---
+++ technical_assignment.dart
@@ -0,0 +1,13 @@
+void main() {
+  int myInteger = 20; //Declares an integer variable named myInteger with a value of 20
+  print("Integer: $myInteger");
+
+  double firstDouble = 5.25; // Declares a double variable named firstDouble with a value of 5.25
+  print("Double: $firstDouble");
+
+String firstString = "This is my first technical assignment"; // Declares a string variable named firstString within it.
+print("String: $firstString");
+
+Map<String, dynamic> myMap = {"name": "Mathews Onyango", "location": "Kisumu-Kenya", "school": "Power Learn Project", "age": 21}; // Declares a map variable named myMap with string keys and its dynamic values
+print("Map: $myMap");
+}
